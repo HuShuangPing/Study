ffbb6f86ba07d924ad7079c6b646203ca14aa307
胡双平 <846818978@qq.com>
2018-05-07 11:13:15 +0800

    Update ssh_发送文件.py

--- day09--异步IO 数据库 队列 缓存/ssh_发送文件.py
+++ day09--异步IO 数据库 队列 缓存/ssh_发送文件.py
@@ -2,7 +2,13 @@
 # -*- coding:utf-8 -*-
 # Author:HuShuangPing
 
+ 
 import paramiko
+
+#基于用户名和密码的 transport 方式登录
+#基于用户名和密码的 sshclient 方式登录是传统的连接服务器、执行命令、关闭的一个操作，
+#有时候需要登录上服务器执行多个操作，比如执行命令、上传/下载文件，则基于用户名和密码的 sshclient 方式无法实现
+
 transport = paramiko.Transport(('10.0.0.31', 52113))
 transport.connect(username='root', password='123456')  #建立链接
 
